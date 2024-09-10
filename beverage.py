e7183aa543a251628c4eb468edf220e8f52f161c
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2024-09-10 17:09:41 +0300

    Добавлены геттеры get_name, get_size, get_price

--- beverage.py
+++ beverage.py
@@ -4,3 +4,12 @@ class Beverage:
         self.__name = name
         self.__size = size
         self.__price = price
+
+    def get_name(self):
+        return self.__name
+
+    def get_size(self):
+        return self.__size
+
+    def get_price(self):
+        return self.__price
